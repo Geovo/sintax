9f386cc972b8f8165c9f18e558d8340298006432
Kristian Voshchepynets <georgeo.voshch@gmail.com>
2016-05-26 16:47:06 +0300

    moved fully to dfa, parsing js numbers well. next step - strings

--- src/sintax.js
+++ src/sintax.js
@@ -2,14 +2,93 @@
  * Author: Kristian Voshchepynets
  *
  */
+
 'use strict';
+var out = "",
+    cache = "",
+    spanOpen = false;
+
+
+function Transition(input, next, exec) {
+    this.input = input;
+    this.next = next;
+    this._exec = exec;
+}
+
+Transition.prototype.run = function(c) {
+    this._exec(c);
+}
+
+function State(id, transitions, halt) {
+    this.id = id;
+    this.transitions = transitions;
+    this.halt = halt;
+}
+
+
+function DFA(states, initial) {
+    this._states = states;
+    this._initial = initial;
+    this.current = initial;
+    //this._currstr = "";
+    //this.out = "";
+}
+
+ DFA.prototype.matcher = function(input, reg) {
+    // console.log("matching: " + input + " with " + reg);
+     if (input == null)
+         return false;
+     if (reg == null)
+         if (input == null)
+             return true;
+         else
+             return false;
+     else
+         return input.match(reg);
+ }
+
+     // make transition here
+DFA.prototype.move = function(inp) {
+    //this._currstr += inp;
+    //console.log("current: " + this.current + " t: " + this._states[0]);
+    var t = this._states[this.current].transitions;
+    var noTransition = true;
+    for (var i = 0; i < t.length; i++) {
+        //console.log(t[i]);
+        if (this.matcher(inp, t[i].input)) {
+            // call function
+            console.log("passing inp: " + inp);
+            t[i].run(inp);
+            this.current = t[i].next;
+            console.log("switched to state: " + this.current);
+            noTransition = false;
+            break;
+        }
+    }
+    if (noTransition) {
+        // then either the dfa doesn't accept or accepts
+        //if (this._states[this.current].halt)
+            // accept
+            console.log("dfa finished");
+            if (spanOpen) {
+                JS.endSpan("");
+                spanOpen = false;
+            }
+        //else
+            // don't accept
+        //    console.log("ERROR: dfa doesn't accept");
+    }
+}
+
+// const skeleton = require("./skeleton.js");
+//const lang = require("./languages.js");
 
 // process text
 function byChar(input) {
     var i = 0;
 
     function hasNext() {
-        return input[i+1] != null ? true : false;
+        return input[i] != null ? true : false;
     }
 
     function next() {
@@ -51,107 +130,156 @@ function generate_css(cols, minify) {
 //console.log(generate_css(colors));
 
 // global out for now
-var out = "",
+/*var out = "",
     cache = "",
     spanOpen = false;
 
-class Transition {
-    constructor(input, next, exec) {
-        this.input = input;
-        this.next = next;
-        this._exec = exec;
-    }
-
-    run(c) {
-        this._exec(c);
-    }
-}
-
-class State {
-    constructor(id, transitions, halt) {
-        this.id = id;
-        this.transitions = transitions;
-        this.halt = halt;
-    }
-}
-
-
-class DFA {
-    constructor(states, initial) {
-        this._states = states;
-        this._initial = initial;
-        this.current = initial;
-        this._currstr = "";
-        this.out = "";
-    }
-
-    matcher(input, reg) {
-        if (input == null)
-            return false;
-        if (reg == null)
-            if (input == null)
-                return true;
-            else
-                return false;
-        else
-            return input.match(reg);
-    }
-
-    // make transition here
-    move(inp) {
-        //this._currstr += inp;
-        var t = this._states[this.current].transitions;
-        //console.log(t);
-        var noTransition = true;
-        for (var i = 0; i < t.length; i++) {
-            if (this.matcher(inp, t[i].input)) {
-                // call function
-                console.log("passing inp: " + inp);
-                t[i].run(inp);
-                this.current = t[i].next;
-                console.log("switched to state: " + this.current);
-                noTransition = false;
-                break;
-            }
-        }
-        if (noTransition) {
-            // then either the dfa doesn't accept or accepts
-            //if (this._states[this.current].halt)
-                // accept
-                console.log("dfa finished");
-                if (spanOpen) {
-                    endSpan("");
-                    spanOpen = false;
-                }
-            //else
-                // don't accept
-            //    console.log("ERROR: dfa doesn't accept");
-        }
-    }
-}
+*/
 
 // states should start with 0 and the initial should be 0
 function sintax(states) {
+    var saved = states;
     var dfa = new DFA(states, 0);
-    var out = "";
+    //var out = "";
 
     function colorize(text) {
         out = "";
+        dfa = new DFA(saved, 0);
         var iter = byChar(text);
         while (iter.hasNext())
             dfa.move(iter.next());
+        if (spanOpen)
+            out += "</span>";
+        console.log("out: " + out)
+        return out;
     }
 
     return {
         dfa: dfa,
-        out: out,
+        //out: out,
         colorize: colorize
     };
 }
 
 
-module.exports = {
-    colorize: sintax,
-    generate_css: generate_css,
-    iterator: byChar
+const JS = {
+    addChar: function(c) {
+        out += c;
+        //console.log("fs2_2: " + c);
+    },
+
+    endSpan: function(c) {
+        out += "</span>" + c;
+        spanOpen = false;
+        //console.log("fs2_3: " + c + "</span>");
+    },
+
+    addSpan: function(c) {
+        out += "<span class='sin_numbers'>" + c;
+        spanOpen = true;
+        //console.log("fs3_4: </span>");
+    },
+
+    notSure: function(c) {
+        cache += "c";
+    },
+
+    clearNotSure: function(c) {
+        cache = c;
+    },
+
+    uncache: function(c) {
+        out += cache + c;
+        cache = "";
+    },
+
+    endSpan_uncache: function(c) {
+        // endSpan("");
+        out += "</span>" + c;
+        spanOpen = false;
+        //uncache(c);
+        out += cache + c;
+        cache = "";
+    },
+    /* regexp's
+    ** Parsing Numbers: */
+    num: /[0-9]/,
+    dot: /\./,
+    notNum: /[^0-9]/,
+    notDot: /[^\.]/,
+    id: /[a-zA-Z_]/,
+    exp: /E/,
+    plusMinus: /[\+\-]/,
+
+    /** Parsing Strings: **/
+    singleQuote: /\'/,
+    doubleQuote: /\"/,
+    backtickQuote: /\`/,
+
+    /**  **/
+
+    reserved: function(word) {
+        return word.match(/var|function|const|let|match|map|toString|class/);
+    }
+    //states: [state1, state2, state3, state4]
+}
+
+const JSStates = {
+    // create dummy states
+    states: [
+        /* Getting a number */
+        new State(0, [
+            new Transition(JS.num, 1, JS.addSpan),
+            new Transition(JS.notNum, 0, JS.addChar)
+        ]),
+        /* Either stop, add further chars or move to parsing float */
+        new State(1, [
+            new Transition(JS.num, 1, JS.addChar),
+            new Transition(JS.dot, 2, JS.addChar),
+            new Transition(JS.notNum, 0, JS.endSpan)
+        ]),
+        /* Add digits after dot or get an E - move to exponent */
+        new State(2, [
+            new Transition(JS.num, 2, JS.addChar),
+            new Transition(JS.exp, 3, JS.addChar),
+            new Transition(JS.notNum, 0, JS.endSpan)
+        ]),
+        new State(3, [
+            /* If not plus or minus after E, but a number, then move on */
+            new Transition(JS.plusMinus, 4, JS.addChar),
+            new Transition(JS.num, 4, JS.addChar),
+            new Transition(JS.notNum, 0, JS.endSpan),
+        ]),
+        new State(4, [
+            new Transition(JS.num, 4, JS.addChar),
+            new Transition(JS.notNum, 0, JS.endSpan),
+        ])
+    ]
 };
+
+//var s = sintax(lang.JS());
+//s.colorize("145.14");
+//console.log(s.out);
+module.exports = {
+    js: JSStates,
+    init: sintax,
+    generate_css: generate_css,
+    iterator: byChar,
+    out: out
+};
+
+/*var sintax = sintax(JSStates.states);
+
+sintax.colorize("145.14");
+console.log(out);*/
+//JS.print()
+
+/*var simple_nums = ["459", "1983.9901", "1.1893124E+45"];
+var dfa = sintax(JSStates.states);
+console.log(dfa);
+for (var i = 0; i < simple_nums.length; i++) {
+    var n = simple_nums[i];
+    //var res = dfa.colorize(n);
+    //test.string(res); // should be a string
+    //test.assert.equal(dfa.colorize(n), "<span class='sin_numbers'>" + n + "</span>");
+}*/
--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,9 @@
 //var assert = require('chai').assert;
+'use strict';
 var test = require('unit.js')
 var sintax = require("../src/sintax.js");
+
+var dfa = sintax.init(sintax.js.states);
 
 var colors = {
     reserved: "#FC29FF",
@@ -18,6 +21,8 @@ span.sin_identifiers {color: #4d9cee;}
 span.sin_special {color: #ffb225;}
 span.sin_comment {color: #777;}
 `;
+
+//console.log(sintax.js);
 
 describe('sintax', function() {
   describe('generate_css()', function () {
@@ -39,11 +44,12 @@ describe('sintax', function() {
      it('should understand numbers', function() {
          // should parse number right
          var simple_nums = ["459", "1983.9901", "1.1893124E+45"];
+         console.log(dfa._states);
          for (var i = 0; i < simple_nums.length; i++) {
              var n = simple_nums[i];
-             var res = sintax.colorize(n);
+             var res = dfa.colorize(n);
              test.string(res); // should be a string
-             test.assert.equal(sintax.colorize(n), "<span class='sin_numbers'>" + n + "</span>");
+             test.assert.equal(dfa.colorize(n), "<span class='sin_numbers'>" + n + "</span>");
          }
      });
      it ('should trim the whitespace', function() {
@@ -51,10 +57,22 @@ describe('sintax', function() {
          var white_nums = ["111   ", "9.1623462363426\t \n","9081.283E-12\n\n\t "];
          for (var i = 0; i < white_nums.length; i++) {
              var n = white_nums[i];
-             var res = sintax.colorize(n);
+             var res = dfa.colorize(n);
              test.string(res); // should be a string
-             console.log("match: " + res + res.match(/[0-9]+\.[0-9]+(E[\+\-][0-9]+)?(![\n\t ]+)/));
+             //console.log("match: " + res + res.match(/[0-9]+\.[0-9]+(E[\+\-][0-9]+)?(![\n\t ]+)/));
              test.assert.strictEqual(res.match(/[0-9]+\.[0-9]+(E[\+\-][0-9]+)?(^[\n\t ]+)/), null);
+         }
+     });
+
+     it ('should recognize simple strings', function() {
+         // should not have any appending whitespace
+         var simple_strings = ["I'm a string", "Stringify me, baby!", "Jelly Beans are really tasty and I mean it! See you in court! :)"];
+         for (var i = 0; i < simple_strings.length; i++) {
+             var n = simple_strings[i];
+             var res = dfa.colorize(n);
+             test.string(res); // should be a string
+             //console.log("match: " + res + res.match(/[0-9]+\.[0-9]+(E[\+\-][0-9]+)?(![\n\t ]+)/));
+             test.assert.notEqual(res.match(/^\<span class=\'sin_strings\'\>.+\<\/span\>$/), null);
          }
      });
 
